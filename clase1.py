575247565c4d87be37d2b187545efefc8127c8e2
Celeste Martinez Chavez <celestemtz0903@gmail.com>
2025-10-06 11:30:45 -0600

    clase1

---
+++ clase1.py
@@ -0,0 +1,13 @@
+class ListaSimple:
+    def __init__(self):
+        self.mi_lista = [3]
+        
+    def obtener_lista(self):
+        return self.mi_lista
+
+if __name__ == "__main__":
+    lista = ListaSimple()
+    print(lista.obtener_lista())
+
+        
+    
